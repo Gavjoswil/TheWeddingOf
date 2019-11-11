dbf415a31a5452dc51004d9bb5811e173d8d5ee8
Gavin Williams <gjosephw24@gmail.com>
2019-11-10 21:39:23 -0500

    Starting on database

---
+++ TheWeddingOf.Data/Rsvp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWeddingOf.Data
+{
+    public class Rsvp
+    {
+        public string Names { get; set; }
+        public string FoodTwo { get; set; }
+        public string FoodOne { get; set; }
+
+    }
+}
