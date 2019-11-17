f6e07d14eb694b4cd53a71bd5dc44e9c3af7427d
Gavin Williams <gjosephw24@gmail.com>
2019-11-17 16:00:43 -0500

    rsvp dbset created

--- TheWeddingOf.Data/Rsvp.cs
+++ TheWeddingOf.Data/Rsvp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,15 @@ namespace TheWeddingOf.Data
 {
     public class Rsvp
     {
+        [Key]
+        public int RsvpId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
         public string Names { get; set; }
-        public string FoodTwo { get; set; }
-        public string FoodOne { get; set; }
-
+        [Required]
+        public int FoodTwo { get; set; }
+        [Required]
+        public int FoodOne { get; set; }
     }
 }
