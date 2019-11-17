858fa95e4b224c3cf4db4f5dbdbe732636f94b2e
Gavin Williams <gjosephw24@gmail.com>
2019-11-17 16:22:33 -0500

    Added rsvp list view

---
+++ TheWeddingOf.Models/RsvpListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWeddingOf.Models
+{
+    public class RsvpListItem
+    {
+        public string Names { get; set; }
+        [Display(Name="Spaghetti")]
+        public int FoodTwo { get; set; }
+        [Display(Name="Ravioli")]
+        public int FoodOne { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ TheWeddingOf.Web/Controllers/RsvpController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheWeddingOf.Models;
+
+namespace TheWeddingOf.Web.Controllers
+{
+    [Authorize]
+    public class RsvpController : Controller
+    {
+        // GET: Rsvp
+        public ActionResult Index()
+        {
+            var model = new RsvpListItem[0];
+            return View(model);
+        }
+    }
+}
