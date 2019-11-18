432e56438b899dc28a842de95f1c872539bc4548
Gavin Williams <gjosephw24@gmail.com>
2019-11-17 20:25:22 -0500

    Starting on create view

---
+++ TheWeddingOf.Models/RsvpCreate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWeddingOf.Models
+{
+    public class RsvpCreate
+    {
+        [Required]
+        public string Names { get; set; }
+        public int FoodTwo { get; set; }
+        public int FoodOne { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
--- TheWeddingOf.Web/Controllers/RsvpController.cs
+++ TheWeddingOf.Web/Controllers/RsvpController.cs
@@ -16,5 +16,10 @@ namespace TheWeddingOf.Web.Controllers
             var model = new RsvpListItem[0];
             return View(model);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
